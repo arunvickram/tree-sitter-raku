63f05798e57d34598dda554d1d703a6fab0f7f8a
Arun <arunvickram@proton.me>
2024-02-20 14:40:38 -0500

    made progress

--- grammar.js
+++ grammar.js
@@ -3,6 +3,152 @@ module.exports = grammar({
 
   rules: {
     // TODO: add the actual grammar 
-    source_file: $ => 'hello'
+    source_file: $ => repeat($._statement),
+
+    _statement: $ => choice(
+      $.class_definition,
+      $.multimethod_definition,
+      $.subroutine_definition,
+      $.return_statement
+    ),
+
+    class_definition: $ => seq(
+      'class',
+      $.identifier,
+      // TODO add is and does declaration
+      $.block
+    ),
+
+    multimethod_definition: $ => seq(
+      'multi',
+      optional('sub'),
+      $.identifier,
+      $.parameter_list,
+      $.block
+    ),
+
+    subroutine_definition: $ => seq(
+      'sub',
+      $.identifier,
+      $.parameter_list,
+      $.block
+    ),
+
+    var_definition: $ => seq(
+      'my',
+      $.identifier,
+      '=',
+      $.identifier
+    ),
+
+    parameter_list: $ => seq(
+      '(',
+      // TODO: parameters
+      ')'
+    ),
+
+    // parameter: $ => choice(
+    //   $.typed_parameter,
+    //   $.untyped_parameter
+    // ),
+
+    // typed_parameter: $ => seq(
+
+    // ),
+
+    block: $ => seq(
+      '{',
+      repeat($._statement),
+      '}',
+    ),
+
+    return_statement: $ => seq(
+      'return',
+      ';'
+    ),
+    // _conditionals: $ => choice('if', 'unless'),
+
+    // subroutine_declaration_statement: $ => seq(
+    //   'sub',
+    //   field('name', $.bareword),
+    //   field('signature', $.signature),
+    //   field('block', $.block)
+    // ),
+
+    // signature: $ => (
+
+    // ),
+
+    // TODO change these to match the actual definitions
+    // identifier: $ => /[\p{L}]+/,
+    extended_identifier: $ => /[\p{L}]+/,
+    compound_identifier: $ => /[\p{L}]+/,
+
+
+    identifier: $ => {
+      const alpha = /[\p{Letter}\p{Mark}]+/;
+      const dashThenAlphanumeric = /-?[\p{Letter}\p{Mark}\p{Number}]+/;
+      const quoteThenAlphanumeric = /'?[\p{Letter}\p{Mark}\p{Number}]+/;
+      return token(seq(alpha, repeat(choice(dashThenAlphanumeric, quoteThenAlphanumeric))));
+    },
+
+    number: $ => /[\p{N}]+/,
+    // identifier: $ => prec(2, $._identifier),
+    // _identifier: $ => /[a-zA-Z_]\w*/,
+
+    // bareword: $ => prec.dynamic(1, $._bareword),
+    // // _bareword is at the very end b/c the lexer prefers tokens defined earlier in the grammar
+    // _bareword: $ => choice($._identifier, /((::)|([a-zA-Z_]\w*))+/), // TODO: unicode
+    // ...primitives,
   }
 })
+
+/**
+ * Creates a rule to match one or more of the rules separated by a comma
+ *
+ * @param {RuleOrLiteral} rule
+ *
+ * @return {SeqRule}
+ *
+ */
+function commaSep1(rule) {
+  return sepBy1(',', rule);
+}
+
+/**
+ * Creates a rule to optionally match one or more of the rules separated by a comma
+ *
+ * @param {RuleOrLiteral} rule
+ *
+ * @return {SeqRule}
+ *
+ */
+function commaSep(rule) {
+  return sepBy(',', rule);
+}
+
+/**
+ * Creates a rule to optionally match one or more of the rules separated by a separator
+ *
+ * @param {RuleOrLiteral} sep
+ *
+ * @param {RuleOrLiteral} rule
+ *
+ * @return {ChoiceRule}
+ */
+function sepBy(sep, rule) {
+  return optional(sepBy1(sep, rule));
+}
+
+/**
+ * Creates a rule to match one or more of the rules separated by a separator
+ *
+ * @param {RuleOrLiteral} sep
+ *
+ * @param {RuleOrLiteral} rule
+ *
+ * @return {SeqRule}
+ */
+function sepBy1(sep, rule) {
+  return seq(rule, repeat(seq(sep, rule)));
+}
---
+++ test.raku
@@ -0,0 +1,3 @@
+sub name() {
+    return;
+}
